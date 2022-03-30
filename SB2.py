468ac4cc4599c4294d89d39b4af3967a5822ea93
MUHAMMAD SOMAIL <101253592+SOMAIL-BRAND@users.noreply.github.com>
2022-03-30 15:11:43 +0500

    Update SB2.py

--- SB2.py
+++ SB2.py
@@ -3,11 +3,11 @@ try:
     import requests
 except:
     os.system('pip install requests')
- 
+os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from slimport reg
+    from sl import reg
     reg()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
